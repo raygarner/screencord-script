7ea641e45f504f9c9fd06b1e6767c90d7673bed1
Ray Garner <ray@perfectcast.co.uk>
2020-02-29 16:08:29 +0000

    takes name param now

--- record.sh
+++ record.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ffmpeg -f x11grab -r 24 -s 1920x1080 -i :0.0 -f pulse -i default out.mkv
+ffmpeg -f x11grab -r 24 -s 1920x1080 -i :0.0 -f pulse -i default $1
 
 #-i :0.0 sets the screen id to record from
-#-i hw:2 sets the audio card to record from
